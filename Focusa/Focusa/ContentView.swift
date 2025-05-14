42fce147f8a95b044b89cc025cf4beddc1e02e4f
Andrey Philippov <andrey.philippov@bookyourhunt.com>
2025-05-14 22:59:26 +0100

    setup project

---
+++ Focusa/Focusa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Focusa
+//
+//  Created by Andrey Philippov on 14.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Focusa/Focusa/FocusaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusaApp.swift
+//  Focusa
+//
+//  Created by Andrey Philippov on 14.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct FocusaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
